53fe1bba06d345c1b16bf89921a2ea5023868e32
MirZhou <mir3732@live.com>
2020-05-07 17:30:26 +0800

    修改代码注释

--- mir.test.token.security/src/main/java/cn/mir/test/token/security/contorller/HomeController.java
+++ mir.test.token.security/src/main/java/cn/mir/test/token/security/contorller/HomeController.java
@@ -5,15 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * <p>Create time: 2020/4/16 22:27</p>
+ * 控制类--首页
+ * <p>
+ * Create time: 2020/4/16 22:27
+ * </p>
  *
  * @author 周光兵
  */
 @Slf4j
 @Controller
 public class HomeController {
-    @GetMapping(value = {"/", "/index"})
+    @GetMapping(value = { "/", "/index" })
     public String index() {
+        log.info("访问系统首页");
+
         return "index";
     }
 }
